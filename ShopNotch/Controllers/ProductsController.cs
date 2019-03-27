aaa7c1f3e47905e2bfc3936f846813e6ff64624a
dtrilsbeek <d.trilsbeek@gmail.com>
2019-03-27 14:10:11 +0100

    Added containers for all product views

--- ShopNotch/Controllers/ProductsController.cs
+++ ShopNotch/Controllers/ProductsController.cs
@@ -85,7 +85,7 @@ namespace ShopNotch.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Sku,Length,Width,Height")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Price,Sku,StockQty,Length,Width,Height")] Product product)
         {
             if (id != product.Id)
             {
