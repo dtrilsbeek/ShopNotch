3d8d6bdf73d82c9e0471aaa096564d856293d31d
dtrilsbeek <d.trilsbeek@gmail.com>
2019-03-27 15:38:17 +0100

    Added categories

---
+++ ShopNotch/Models/CreateViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ShopNotch.Models
+{
+	public class CreateViewModel
+	{
+		public IEnumerable<Category> Categories { get; private set; }
+		public IEnumerable<Product> Products { get; private set; }
+
+		public string Name { get; set; }
+		public int? ParentId { get; set; }
+		public List<SelectListItem> CategoryNames { get; set; }
+
+		public CreateViewModel(List<Category> categories, List<Product> products)
+		{
+			Categories = categories;
+			Products = products;
+		}
+	}
+}
