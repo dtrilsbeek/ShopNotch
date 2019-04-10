073c7bdb131749bafc0dbc9cf9ef50db0ccddfe6
dtrilsbeek <d.trilsbeek@gmail.com>
2019-04-10 15:03:47 +0200

    added product queries

--- Data/Contexts/BaseDb.cs
+++ Data/Contexts/BaseDb.cs
@@ -1,25 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Data.Contexts
 {
 	public abstract class BaseDb<TEntity>
 	{
-		protected IEnumerable<TEntity> GetAll(IDbCommand command)
+		private string _connectionString = "Server=mssql.fhict.local;Database=dbi391176_elayed;User Id=dbi391176_elayed;Password=appelsenperen12;";
+
+		protected IEnumerable<TEntity> ExecuteQuery(string query)
 		{
-			using (var reader = command.ExecuteReader())
+			using (SqlConnection connection = new SqlConnection(_connectionString))
 			{
-				List<TEntity> items = new List<TEntity>();
-				while (reader.Read())
+				SqlCommand command = new SqlCommand(query, connection);
+				connection.Open();
+				SqlDataReader reader = command.ExecuteReader();
+				try
 				{
-					var item = CreateEntity();
-					Map(reader, item);
-					items.Add(item);
+					List<TEntity> items = new List<TEntity>();
+					while (reader.Read())
+					{
+						var item = CreateEntity();
+						Map(reader, item);
+						items.Add(item);
+					}
+
+					return items;
 				}
-				return items;
+				finally
+				{
+					reader.Close();
+				}
 			}
+		}
+
+		protected bool ExecuteNonQuery(string query)
+		{
+			using (SqlConnection connection = new SqlConnection(_connectionString))
+			{
+				SqlCommand command = new SqlCommand(query, connection);
+				connection.Open();
+				try
+				{
+					int result = command.ExecuteNonQuery();
+
+					if (result == 0)
+					{
+						return false;
+					}
+				}
+				catch (Exception ex)
+				{
+					return false;
+				}
+				return true;
+			}
+		}
+
+		protected static T ConvertFromDbVal<T>(object obj)
+		{
+			if (obj == null || obj == DBNull.Value)
+			{
+				return default(T);
+			}
+
+			return (T)obj;
 		}
 
 		protected abstract void Map(IDataRecord record, TEntity entity);
--- Data/Contexts/ProductSqlContext.cs
+++ Data/Contexts/ProductSqlContext.cs
@@ -1,37 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
-using System.Text;
 using Data.Interfaces;
 using Data.Models;
 
 namespace Data.Contexts
 {
-	public class ProductSqlContext : BaseDb<Product>, IContext
+	public class ProductSqlContext : BaseDb<Product>, IRepository<Product>
 	{
-		public IEnumerable<IEntity> GetAll()
+		public IEnumerable<Product> GetAll()
 		{
-			return GetAll(new SqlCommand("SELECT * FROM Products"));
-		}
-
-		protected override void Map(IDataRecord record, Product entity)
-		{
-			entity.Id = (int)record["Id"];
-			entity.Name = (string)record["Name"];
-			entity.Description = (string)record["Description"];
-			entity.Price = (decimal)record["Price"];
-			entity.Sku = (string)record["Sku"];
-			entity.Length = (double)record["Length"];
-			entity.Width = (double)record["Width"];
-			entity.Height = (double)record["Height"];
-			entity.StockQty = (int)record["StockQty"];
-			entity.Weight = (double)record["Weight"];
+			return ExecuteQuery("SELECT * FROM Product");
 		}
 
 		protected override Product CreateEntity()
 		{
 			return new Product();
+		}
+
+		public void Add(Product entity)
+		{
+			bool result = ExecuteNonQuery(
+				"INSERT INTO Product" +
+				$"(Name, Description, Price, Sku, Length, Width, Height, StockQty, Weight)" +
+				$"WHERE Id = {entity.Id}");
+		}
+
+		public void Delete(Product entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Update(Product entity)
+		{
+			bool result = ExecuteNonQuery(
+				"UPDATE Product" +
+				$"SET Name = {entity.Name}, " +
+				$"Description = {entity.Description}, " +
+				$"Price = {entity.Price}, " +
+				$"Sku = {entity.Sku}, " +
+				$"Length = {entity.Length}, " +
+				$"Width = {entity.Width}, " +
+				$"Height = {entity.Height}, " +
+				$"StockQty = {entity.StockQty}, " +
+				$"Weight = {entity.Weight}, " +
+				$"WHERE Id = {entity.Id}");
+		}
+
+		public Product FindById(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		protected override void Map(IDataRecord record, Product entity)
+		{
+			entity.Id = (int)record["Id"];
+			entity.Name = ConvertFromDbVal<string>(record["Name"]);
+			entity.Description = ConvertFromDbVal<string>(record["Description"]);
+			entity.Price = ConvertFromDbVal<decimal>(record["Price"]);
+			entity.Sku = ConvertFromDbVal<string>(record["Sku"]);
+			entity.Length = ConvertFromDbVal<double>(record["Length"]);
+			entity.Width = ConvertFromDbVal<double>(record["Width"]);
+			entity.Height = ConvertFromDbVal<double>(record["Height"]);
+			entity.StockQty = ConvertFromDbVal<int>(record["StockQty"]);
+			entity.Weight = ConvertFromDbVal<double>(record["Weight"]);
 		}
 	}
 }
