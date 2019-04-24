add6e4dc370ba5559c563c141224f6ce02876d37
dtrilsbeek <d.trilsbeek@gmail.com>
2019-04-24 13:26:48 +0200

    added categorytree classes

---
+++ Logic/Category/CategoryNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Models;
+
+namespace Logic
+{
+	class Node
+	{
+		public List<Node> SubNodes { get; set; }
+
+		public Category Category { get; set; }
+	}
+}
---
+++ Logic/Category/CategoryTree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Models;
+
+namespace Logic
+{
+	class CategoryTree
+	{
+		public List<Node> MainNodes { get; set; }
+
+		public CategoryTree(List<Category> categories)
+		{
+			MainNodes = new List<Node>();
+			CreateMainNodes(categories);
+		}
+
+		private void CreateMainNodes(List<Category> categories)
+		{
+			foreach (Category category in categories)
+			{
+				
+			}
+		}
+	}
+}
