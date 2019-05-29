f50a7b92c5a7e83ad81cd05d7243b0a858b9b146
dtrilsbeek <d.trilsbeek@gmail.com>
2019-05-29 15:02:47 +0200

    added stored procedure

---
+++ SQL/Stored Procedures/dbo.GetCategoriesWithParent.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetCategoriesWithParent]
+	
+AS
+	SELECT  child.Id, child.Name, 
+		parent.Id AS ParentId,  
+		parent.Name AS ParentName  
+	FROM Category AS child  
+	LEFT JOIN Category AS parent ON child.ParentId = parent.Id
+				
+GO;
+
+
