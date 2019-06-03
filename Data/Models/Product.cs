5c4b81899696faf3209d096f26510615bff91c62
dtrilsbeek <d.trilsbeek@gmail.com>
2019-06-03 15:37:44 +0200

    removed unused

--- Data/Models/Product.cs
+++ Data/Models/Product.cs
@@ -6,13 +6,7 @@ namespace Data.Models
 {
     public partial class Product : IEntity
     {
-        public Product()
-        {
-            CategoryPerProduct = new HashSet<CategoryPerProduct>();
-            ProductPerOrder = new HashSet<ProductPerOrder>();
-        }
-
-        public int Id { get; set; }
+	    public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
@@ -22,8 +16,5 @@ namespace Data.Models
         public double? Height { get; set; }
         public int? StockQty { get; set; }
         public double? Weight { get; set; }
-
-        public virtual ICollection<CategoryPerProduct> CategoryPerProduct { get; set; }
-        public virtual ICollection<ProductPerOrder> ProductPerOrder { get; set; }
     }
 }
