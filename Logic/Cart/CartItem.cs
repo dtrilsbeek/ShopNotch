490452bbaabb334650267f16de3fed905131dc3f
dtrilsbeek <d.trilsbeek@gmail.com>
2019-06-13 18:43:32 +0200

    added cartlogic

---
+++ Logic/Cart/CartItem.cs
@@ -0,0 +1,11 @@
+﻿using Data.Interfaces;
+
+namespace Logic.Cart
+{
+	public class CartItem : IEntity
+	{
+		public int Id { get; }
+		public int ProductId { get; set; }
+		public int Amount { get; set; }
+	}
+}
---
+++ Logic/CartLogic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Logic.Cart;
+using Logic.Interfaces;
+
+namespace Logic
+{
+	public class CartLogic : ILogic<CartItem>
+	{
+		public IEnumerable<CartItem> GetAll()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Add(CartItem entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Delete(CartItem entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Update(CartItem entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public CartItem GetById(int id)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
