ed811a0dd362cbd1b5eab7eb0114e4d22637c531
dtrilsbeek <d.trilsbeek@gmail.com>
2019-06-17 13:35:49 +0200

    added integradion tests

---
+++ IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace IntegrationTests
+{
+	public class IntegrationTests
+		: IClassFixture<WebApplicationFactory<ShopNotch.Startup>>
+	{
+		private readonly WebApplicationFactory<ShopNotch.Startup> _factory;
+
+		public IntegrationTests(WebApplicationFactory<ShopNotch.Startup> factory)
+		{
+			_factory = factory;
+		}
+
+		[Theory]
+		[InlineData("/")]
+		[InlineData("/Shop/Index")]
+		[InlineData("/Shop/Categories")]
+		[InlineData("/Shop/Cart")]
+		[InlineData("/Notch")]
+		[InlineData("/Notch/Products")]
+		[InlineData("/Notch/Products/Edit/1")]
+		[InlineData("/Notch/Products/Details/1")]
+		[InlineData("/Notch/Products/Delete/1")]
+		[InlineData("/Notch/Categories")]
+		[InlineData("/Notch/Categories/Edit/2")]
+		[InlineData("/Notch/Categories/Delete/2")]
+		public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+		{
+			// Arrange
+			var client = _factory.CreateClient();
+
+			// Act
+			var response = await client.GetAsync(url);
+
+			// Assert
+			response.EnsureSuccessStatusCode(); // Status Code 200-299
+			Assert.Equal("text/html; charset=utf-8",
+				response.Content.Headers.ContentType.ToString());
+		}
+	}
+}
