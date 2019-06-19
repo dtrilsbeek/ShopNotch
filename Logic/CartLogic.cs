c861343b963a9e5afb07993297a7127eea35022e
dtrilsbeek <d.trilsbeek@gmail.com>
2019-06-19 20:48:15 +0200

    updated cart logic

--- Logic/CartLogic.cs
+++ Logic/CartLogic.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-//using Logic.Cart;
+using Data.Models;
 using Logic.Interfaces;
 
 namespace Logic
@@ -7,15 +7,34 @@ namespace Logic
 	public class CartLogic : ICartLogic
 	{
 		private Dictionary<int, int> items;
+		private IProductLogic _productLogic;
 
-		public CartLogic()
+		public CartLogic(IProductLogic productLogic)
 		{
+			_productLogic = productLogic;
 			items = new Dictionary<int, int>();
 		}
 
 		public Dictionary<int, int> GetAll()
 		{
 			return items;
+		}
+
+		public Dictionary<Product, int> GetAllProduct()
+		{
+			var products = new Dictionary<Product, int>();
+
+			foreach (var cartItem in items)
+			{
+				var product = _productLogic.GetById(cartItem.Key);
+
+				if (product != null)
+				{
+					products[product] = cartItem.Value;
+				}
+			}
+
+			return products;
 		}
 
 		public void Add(int productId, int amount)
